63b843a85d77249abd55b1bd6ed36701e4708328
tiwavaldese <tiwaval7@gmail.com>
2023-09-22 09:36:37 +0100

    script.py

--- script.py
+++ script.py
@@ -2,23 +2,28 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv('bank_marketing.csv')
+#split the three tables client,campaign & economics with their respective columns
 client = df[["client_id", "age", "job", "marital", "education", 
              "credit_default", "housing", "loan"]]
 campaign = df[["client_id","campaign", "month", "day", 
                "duration", "pdays", "previous", "poutcome", "y"]]
 economics = df[["client_id","emp_var_rate", "cons_price_idx", 
                 "euribor3m", "nr_employed"]]
+#Renaming few columns
 client.rename(columns = {'client_id':id}, inplace =True)
 campaign.rename(columns = {'duration':'contact_duration','previous':'previous_campaign_contacts','y':'campaign_outcome','poutcome':'previous_outcome','campaign':'number_contacts'}, inplace = True)
 economics.rename(columns ={'euribor3m':'euribor_three_months','nr_employed':'number_employed'},inplace = True)
 client['education'] = client['education'].str.replace('.','_')
 client['education'] = client['education'].replace('unknown',np.NAN)
 df['job'] = df['job'].str.replace('.','')
+# Convert previous_outcome & campaign_outcome to binary values
 campaign['previous_outcome'] = campaign['previous_outcome'].map({'sucess': 1,'failure': 0})
 campaign['previous_outcome'] = campaign['previous_outcome'].replace('nonexistance',np.NAN)
 campaign['campaign_outcome'] = campaign['campaign_outcome'].map({'sucess': 1,'failure': 0})
+#create a new column in campaign with all the row having 1
 values = 1
 campaign['campaign_id'] = values
+#create the year column
 campaign['year'] = '2022'
 # Capitalize month column
 campaign["month"] = campaign["month"].str.capitalize()
@@ -29,10 +34,12 @@ campaign['last_contact_date'] = pd.read_csv(campaign['day']) + '-' + campaign['m
 # Convert to datetime
 campaign['last_contact_date'] = pd.to_datetime(campaign['last_contact_date'], format ='%d-%b-%Y')
 client.drop(columns = ['month','day','year'], inplace= True)
-client.csv('client.csv',index =False)
-campaign.csv('campaign.csv',index =False)
-economics.csv('economics.csv',index =False)
-client_table =""" CREATE TABLE client
+#save the tables to their csv files
+client.to_csv('client.csv',index =False)
+campaign.to_csv('campaign.csv',index =False)
+economics.to_csv('economics.csv',index =False)
+# Store and print database_design
+client_table = """CREATE TABLE client
 (id,
 age,
 job,
@@ -43,7 +50,7 @@ housing,
 loan)
 \copy client from 'client.csv' DELIMITER ',' CSV HEADER
 """
-campaign_table =""" CREATE TABLE campaign
+campaign_table = """CREATE TABLE campaign
 (client_id,
 campaign,
 month,
@@ -55,7 +62,7 @@ number_contacts
 )
 \copy campaign from 'campaign.csv' DELIMITER ',' CSV HEADER
 """
-economics_table =""" CREATE TABLE economics
+economics_table = """CREATE TABLE economics
 (client_id,
 emp_var_rate,
 cons_price_idx,
